a174182cf51111e71c5361ca36176d14c973a7c0
Nick Panteleeff <drnix@chess-online.com>
2017-07-25 18:45:53 +0300

    Add export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 export { Color } from './chess/Color';
 export { Castle } from './chess/Castle';
 export { Piece } from './chess/Piece';
+export { Square } from './chess/Square';
 export { Move } from './chess/Move';
 export { SimpleMove } from './chess/SimpleMove';
-export { Position } from './chess/Position';
+export { Position, FenStandartStart, FenEmptyBoard } from './chess/Position';
 export { Chess } from './chess/Chess';
