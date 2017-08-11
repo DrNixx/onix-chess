7044e56134da7aabf99577d888fd53a81f63d94b
DrNix <drnix@chess-online.com>
2017-08-11 03:26:39 +0300

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Intl as IntlChess } from './Intl';
+export { Intl } from './Intl';
 export { Color } from './chess/Color';
 export { Castle } from './chess/Castle';
 export { Piece } from './chess/Piece';
