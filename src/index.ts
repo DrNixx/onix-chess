d960b519159c74f6949631ae7a10437502c3098f
DrNixx <drnixx@gmail.com>
2020-03-10 03:41:50 +0300

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { Intl } from './Intl';
-export { Color } from './chess/Color';
+export { Color, ColorName } from './chess/Color';
 export { Castle } from './chess/Castle';
 export { Piece } from './chess/Piece';
 export { Square } from './chess/Square';
